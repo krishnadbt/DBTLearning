e17d658eede88eed916d7eebb15267974d6e127d
krishnadbt2@gmail.com <krishnadbt2@gmail.com>
2023-06-27 13:19:35 +0000

    dfdsf

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -4,17 +4,15 @@
     )
 }}
 
+with customers as (
+  
+   select * from  {{ ref ('stg_customers') }}
 
+),
 
-orders as (
+with orders as (
 
-    select 
-        id as order_id,
-        user_id as customer_id,
-        order_Date,
-        status
-    from raw.jaffle_shop.orders
-
+    select * from {{ref ('stg_orders') }}
 ),
 
 customer_orders as (
